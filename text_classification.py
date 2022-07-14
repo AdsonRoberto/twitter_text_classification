8fb6b56780c9c62609e184d36be525e4592f23fb
AdsonRoberto <adson.uece@gmail.com>
2022-07-14 16:03:52 -0300

    instalando spacy e importando  bibliotecas

---
+++ text_classification.py
@@ -0,0 +1,14 @@
+!pip install -q spacy==2.2.3
+
+import spacy
+spacy.__version__
+
+!python3 -m spacy download pt
+
+import pandas as pd
+import string
+import spacy
+import random
+import seaborn as sns
+import numpy as np
+import re
