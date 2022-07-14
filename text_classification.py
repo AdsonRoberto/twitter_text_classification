f9bb3a5359bafe813f5b3a9b3a6ae150c6c8aecd
AdsonRoberto <adson.uece@gmail.com>
2022-07-14 17:18:36 -0300

    pre-processamento dos textos

--- text_classification.py
+++ text_classification.py
@@ -45,3 +45,51 @@ base_teste.drop(['id', 'tweet_date', 'query_used'], axis = 1, inplace=True)
 base_teste.head()
 
 sns.heatmap(pd.isnull(base_teste));
+
+pln = spacy.load('pt')
+pln
+
+base_treinamento['tweet_text'][1]
+
+stop_words = spacy.lang.pt.stop_words.STOP_WORDS
+
+print(stop_words)
+
+string.punctuation
+
+def preprocessamento(texto):
+  # Letras minúsculas
+  texto = texto.lower()
+
+  # Nome do usuário
+  texto = re.sub(r"@[A-Za-z0-9$-_@.&+]+", ' ', texto)
+
+  # URLs
+  texto = re.sub(r"https?://[A-Za-z0-9./]+", ' ', texto)
+
+  # Espaços em branco
+  texto = re.sub(r" +", ' ', texto)
+
+  # Emoticons
+  lista_emocoes = {':)': 'emocaopositiva',
+                   ':d': 'emocaopositiva',
+                   ':(': 'emocaonegativa'}
+  for emocao in lista_emocoes:
+    texto = texto.replace(emocao, lista_emocoes[emocao])
+
+  # Lematização
+  documento = pln(texto)
+
+  lista = []
+  for token in documento:
+    lista.append(token.lemma_)
+  
+  # Stop words e pontuações
+  lista = [palavra for palavra in lista if palavra not in stop_words and palavra not in string.punctuation]
+  lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+  
+  return lista
+
+texto_teste = '@behin_d_curtain :D Para :( mim, http://www.iaexpert.com.br é precisamente o contrário :) Vem a chuva e vem a boa disposição :)'
+resultado = preprocessamento(texto_teste)
+resultado
